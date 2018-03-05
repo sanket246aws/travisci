679f40620ca1f6fcddb15377841d200e7c516c25
Sanket Chintal <sanket@247software.in>
2018-03-05 16:43:22 +0530

    12

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -6,7 +6,7 @@ vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
     topic: seriousCalculations.performSeriousCalculations(4),
     'result should be valid': function (result) {
-      assert.isNumber(result);;
+      assert.isNumber(result);
       assert.equal(result, 8);
     }
   }
